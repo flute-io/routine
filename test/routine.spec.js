1d6758e4c4a2bceccab4a42dfec201b750893c80
ifeanyiisitor <ifeanyiisitor@gmail.com>
2016-07-10 16:40:35 +0100

    allow routines to be declared with funcitons that return promises alongside regular functions

--- test/routine.spec.js
+++ test/routine.spec.js
@@ -3,11 +3,15 @@ import {expect} from 'chai';
 
 describe('Routine', function () {
 
-	it('should allow for setting up a routine that consists of ordered stateful function calls', function () {
+	let state;
 
-		const state = {
+	beforeEach(function () {
+		state = {
 			count: 0
 		};
+	});
+
+	it('should allow for setting up a routine that consists of ordered stateful function calls', function () {
 
 		Routine
 			.set({state})
@@ -17,6 +21,48 @@ describe('Routine', function () {
 			.run();
 
 		expect(state).to.eql({count: 3});
+	});
+
+	it('should also support functions that return promises alongside regular functions', function (done) {
+		const promise = Routine
+			.set({state})
+			.first(addOneToCount)
+			.then(addThreeToCount)
+			.then(asynchronouslyAddTwoToCount)
+			.then(subtractOneFromCount)
+			.run();
+
+		expect(promise).to.be.instanceOf(Promise);
+
+		promise.then(()=> {
+			expect(state).to.eql({count: 5});
+		}).then(done);
+	});
+
+	it('should support promise returning function that has been specified as the last action of the routine ', function (done) {
+		Routine
+			.set({state})
+			.first(addOneToCount)
+			.then(addThreeToCount)
+			.then(asynchronouslyAddTwoToCount)
+			.run()
+			.then(()=> {
+				expect(state).to.eql({count: 6});
+			})
+			.then(done);
+	});
+
+	it('should support promise returning function that has been specified as the first action of the routine ', function (done) {
+		Routine
+			.set({state})
+			.then(asynchronouslyAddTwoToCount)
+			.then(addOneToCount)
+			.then(addThreeToCount)
+			.run()
+			.then(()=> {
+				expect(state).to.eql({count: 6});
+			})
+			.then(done);
 	});
 });
 
@@ -30,4 +76,13 @@ function addThreeToCount () {
 
 function subtractOneFromCount () {
 	this.state.count = this.state.count - 1;
+}
+
+function asynchronouslyAddTwoToCount () {
+	const state = this.state;
+	return Promise
+		.resolve()
+		.then(()=> {
+			state.count = state.count + 2;
+		});
 }
