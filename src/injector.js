1b1d673e47c272c42abb9a0d1a52be7762a9d54a
ifeanyiisitor <ifeanyiisitor@gmail.com>
2016-07-30 19:05:36 +0100

    add the ability to specify dependency injection mappings

--- src/injector.js
+++ src/injector.js
@@ -2,12 +2,29 @@ export default function injector (routine) {
 
 	routine.on('before-invoking-operation', (invocation) => {
 
+		const metadata = routine.metadata.get(injector);
+		const mappings = metadata ? metadata.mappings || {} : {};
 		const argNames = $args(invocation.operation);
 		const argsToInject = [];
 
 		for (let argName of argNames) {
 			if (argName === 'routine') {
 				argsToInject.push(routine);
+			}
+			else if (mappings[argName]) {
+				const arg = getItemFromObjByStringPath({
+					obj: routine.scope,
+					path: mappings[argName]
+				});
+
+				if (!arg) {
+					// eslint-disable-next-line quotes
+					throw new Error(`Injector mapping resolution error - mapping value ` +
+						`of '${mappings[argName]}' for dependency named '${argName}' of operation ` +
+						`named '${invocation.operation.name}' does not exist in scope`);
+				}
+
+				argsToInject.push(arg);
 			}
 			else if (routine.scope[argName]) {
 				argsToInject.push(routine.scope[argName]);
@@ -24,6 +41,12 @@ export default function injector (routine) {
 	});
 }
 
+injector.mappings = function (mappings) {
+	return (routine) => {
+		routine.metadata.set(injector, {mappings});
+	};
+};
+
 function $args (func) {
 	return (func + '')
 		.replace(/[/][/].*$/mg, '') // strip single-line comments
@@ -32,4 +55,18 @@ function $args (func) {
 		.split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters
 		.replace(/=[^,]+/g, '') // strip any ES6 defaults
 		.split(',').filter(Boolean); // split & filter [""]
+}
+
+function getItemFromObjByStringPath ({obj, path} = {}) {
+	const props = path.split('.');
+	let item = obj;
+
+	for (let prop of props) {
+		item = item[prop];
+		if (!item) {
+			break;
+		}
+	}
+
+	return item;
 }
--- test/specs/Injector.spec.js
+++ test/specs/Injector.spec.js
@@ -6,7 +6,7 @@ import injector from '../../src/injector';
 
 describe('Routine.use(injector)', function () {
 
-	it('should allow for auto-injecting operation arguments from the scope', function () {
+	it(' - should allow for auto-injecting operation arguments from the scope', function () {
 
 		const state = {
 			count: 0
@@ -20,8 +20,32 @@ describe('Routine.use(injector)', function () {
 
 		expect(state).to.eql({count: 1});
 	});
+
+	it('should allow for specifying mappings that the injector ' +
+		'can use to resolve dependencies', function () {
+
+		const state = {
+			count: 2,
+			amount: 3
+		};
+
+		Routine
+			.use(injector)
+			.use(injector.mappings({
+				amt: 'state.amount'
+			}))
+			.setScopeTo({state})
+			.then(addAmountToCount)
+			.run();
+
+		expect(state).to.eql({count: 5, amount: 3});
+	});
 });
 
 function addOneToStateCount (state) {
 	state.count++;
+}
+
+function addAmountToCount (state, amt) {
+	state.count = state.count + amt;
 }
